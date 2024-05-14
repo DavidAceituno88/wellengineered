17f134368384abf8bedcb3ea664e9e09cb21842c
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2024-05-14 15:58:22 -0700

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,10 +1,18 @@
 
 # Import Module  
 import tabula
+import os
   
 # Read PDF File 
 # this contain a list 
 file_path = r"C:\Users\David\Downloads\Phase 3\roofing_tbl.pdf"
-destination_folter = r"C:\Users\David\Downloads\Phase 3"
-df = tabula.read_pdf(file_path, pages ='all')[0] 
-  
+output_directory = r"C:\Users\David\Downloads\Phase 3\excel"
+
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+output_excel_filename = "roofing.csv"
+
+output_excel_path = os.path.join(output_directory, output_excel_filename)
+
+tabula.convert_into(file_path, output_excel_path, output_format="csv", pages='all')
