fd52c2b8e67388d6fe356c4b43c65c7ee1259fe8
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2024-05-16 15:59:03 -0700

    Update converter.py

--- converter.py
+++ converter.py
@@ -2,17 +2,30 @@
 # Import Module  
 import tabula
 import os
-  
-# Read PDF File 
-# this contain a list 
+from os import listdir
+from os.path import isfile, join
+
+
+dirpath= "C:\\Users\\David\\Downloads\\Phase 3\\"
 file_path = r"C:\Users\David\Downloads\Phase 3\roofing_tbl.pdf"
 output_directory = r"C:\Users\David\Downloads\Phase 3\excel"
+
+files = [f for f in listdir(dirpath) if isfile(join(dirpath, f))]
+
+print(files)
+
+#Extracting the file name
+sub1 = "C:\\Users\\David\\Downloads\\Phase 3\\"
+sub2 = ".pdf"
+
+# getting elements in between using split() and join()
+file_name = ''.join(file_path.split(sub1)[1].split(sub2)[0])
+file_name=  file_name +'.csv'
 
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
-output_excel_filename = "roofing.csv"
 
-output_excel_path = os.path.join(output_directory, output_excel_filename)
+output_excel_path = os.path.join(output_directory, file_name)
 
 tabula.convert_into(file_path, output_excel_path, output_format="csv", pages='all')
