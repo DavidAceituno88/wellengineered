c0f55daf0a426a7bf104dd7fb259b5c080b92aef
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2024-05-20 09:01:51 -0700

    Update converter.py

--- converter.py
+++ converter.py
@@ -6,26 +6,28 @@ from os import listdir
 from os.path import isfile, join
 
 
-dirpath= "C:\\Users\\David\\Downloads\\Phase 3\\"
-file_path = r"C:\Users\David\Downloads\Phase 3\roofing_tbl.pdf"
+dirpath= "C:\\Users\\David\\Downloads\\Phase 3\\originals\\"
+#file_path = r"C:\Users\David\Downloads\Phase 3\roofing_tbl.pdf"
 output_directory = r"C:\Users\David\Downloads\Phase 3\excel"
+
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
 
 files = [f for f in listdir(dirpath) if isfile(join(dirpath, f))]
 
 print(files)
 
 #Extracting the file name
-sub1 = "C:\\Users\\David\\Downloads\\Phase 3\\"
+sub1 = "C:\\Users\\David\\Downloads\\Phase 3\\originals\\"
 sub2 = ".pdf"
 
 # getting elements in between using split() and join()
-file_name = ''.join(file_path.split(sub1)[1].split(sub2)[0])
-file_name=  file_name +'.csv'
+for file_path in files:
+    sub1 = "C:\\Users\\David\\Downloads\\Phase 3\\originals\\"
+    sub2 = ".pdf"
+    file_name = ''.join(file_path.split(sub2)[0])
+    file_name=  file_name +'.csv'
+    output_excel_path = os.path.join(output_directory, file_name)
+    tabula.convert_into(file_path, output_excel_path, output_format="csv", pages='all')
 
-if not os.path.exists(output_directory):
-    os.makedirs(output_directory)
 
-
-output_excel_path = os.path.join(output_directory, file_name)
-
-tabula.convert_into(file_path, output_excel_path, output_format="csv", pages='all')
